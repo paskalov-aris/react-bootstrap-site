eb0f76e7ca51c4204da535599678026f2ad93eb4
Aristotel Paskalov <paskalov.aris@gmail.com>
2025-05-23 22:51:48 +0300

    add cards to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,60 @@
+import { Card, Container, Button } from "react-bootstrap";
 import { CarouselBoxHk } from "../components/CarouselBoxHk";
+
+import DevelopersImg from '../assets/developers.jpg'
+import NaturalScientistsImg from '../assets/natural-scientists.jpg'
+import MarketersImg from '../assets/marketers.jpg'
 
 export const Home = () => {
   return (
     <div>
       <CarouselBoxHk />
+      <Container>
+        <h2 className="text-center m-4">Our team</h2>
+        <div className="row">
+          <div className="col">
+            <Card className="m-4 text-center" bg="light" border="primary">
+              <Card.Img
+                variant="top"
+                src={DevelopersImg}
+              />
+              <Card.Body>
+                <Card.Title>Розробники</Card.Title>
+                <Card.Text>Команда 1</Card.Text>
+                <Button variant="primary">About team</Button>
+              </Card.Body>
+            </Card>
+          </div>
+
+          <div className="col">
+            <Card className="m-4 text-center" bg="light" border="primary">
+              <Card.Img
+                variant="top"
+                src={NaturalScientistsImg}
+              />
+              <Card.Body>
+                <Card.Title>Природознавці</Card.Title>
+                <Card.Text>Команда 2</Card.Text>
+                <Button variant="primary">About team</Button>
+              </Card.Body>
+            </Card>
+          </div>
+
+          <div className="col">
+            <Card className="m-4 text-center" bg="light" border="primary">
+              <Card.Img
+                variant="top"
+                src={MarketersImg}
+              />
+              <Card.Body>
+                <Card.Title>Маркетологи</Card.Title>
+                <Card.Text>Команда 3</Card.Text>
+                <Button variant="primary">About team</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 };
