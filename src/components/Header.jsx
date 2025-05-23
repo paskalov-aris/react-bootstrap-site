bd1e7ecc1c18227f8a2cb4ff515aa82c7eb8fa2f
Aristotel Paskalov <paskalov.aris@gmail.com>
2025-05-23 20:07:23 +0300

    set up react-bootstrap and create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import {
+  Navbar,
+  Nav,
+  FormControl,
+  Container,
+  Form,
+  Button,
+} from "react-bootstrap";
+
+import logo from "../assets/react.svg";
+
+export const Header = () => {
+  return (
+    <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="/">
+          <img src={logo} alt="React logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/"> Home </Nav.Link>
+            <Nav.Link href="/about"> About us </Nav.Link>
+            <Nav.Link href="/contacts"> Contacts </Nav.Link>
+            <Nav.Link href="/blog"> Blog </Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <FormControl type="text" placeholder="Search" className="me-sm-3" />
+            <Button variant="outline-info">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
