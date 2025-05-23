9c0e2fd2b228ec67d52a23429e68d9cdc6749a1d
Aristotel Paskalov <paskalov.aris@gmail.com>
2025-05-23 20:37:54 +0300

    set up react-router-dom and basic pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,30 +6,49 @@ import {
   Form,
   Button,
 } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "../pages/Home";
+import { About } from "../pages/About";
+import { Contacts } from "../pages/Contacts";
+import { Blog } from "../pages/Blog";
 
 import logo from "../assets/react.svg";
 
 export const Header = () => {
   return (
-    <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="/">
-          <img src={logo} alt="React logo" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/"> Home </Nav.Link>
-            <Nav.Link href="/about"> About us </Nav.Link>
-            <Nav.Link href="/contacts"> Contacts </Nav.Link>
-            <Nav.Link href="/blog"> Blog </Nav.Link>
-          </Nav>
-          <Form className="d-flex">
-            <FormControl type="text" placeholder="Search" className="me-sm-3" />
-            <Button variant="outline-info">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="React logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/"> Home </Nav.Link>
+              <Nav.Link href="/about"> About us </Nav.Link>
+              <Nav.Link href="/contacts"> Contacts </Nav.Link>
+              <Nav.Link href="/blog"> Blog </Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="me-sm-3"
+              />
+              <Button variant="outline-info">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </Router>
+    </>
   );
 };
