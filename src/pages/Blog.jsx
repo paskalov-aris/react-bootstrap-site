ce3de4518fbcc5aa18417e87c843fa71630c1567
Aristotel Paskalov <paskalov.aris@gmail.com>
2025-05-23 23:03:47 +0300

    implement blog page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,3 +1,93 @@
+import { Card, Col, ListGroup, Row } from "react-bootstrap";
+
 export const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <Row>
+      <Col md="6">
+        <div className="d-flex align-items-center me-5">
+          <div className="flex-shrink-0">
+            <img
+              width={150}
+              height={150}
+              className="mr-3"
+              src="https://emgotas.files.wordpress.com/2016/11/what-is-a-team.jpg"
+              alt="photo"
+            />
+          </div>
+          <div className="flex-grow-1 ms-3">
+            <h5>Blog post</h5>
+            <p>Lorem</p>
+          </div>
+        </div>
+
+        <div className="d-flex align-items-center me-5">
+          <div className="flex-shrink-0">
+            <img
+              width={150}
+              height={150}
+              className="mr-3"
+              src="https://emgotas.files.wordpress.com/2016/11/what-is-a-team.jpg"
+              alt="photo"
+            />
+          </div>
+          <div className="flex-grow-1 ms-3">
+            <h5>Blog post</h5>
+            <p>Lorem</p>
+          </div>
+        </div>
+
+        <div className="d-flex align-items-center me-5">
+          <div className="flex-shrink-0">
+            <img
+              width={150}
+              height={150}
+              className="mr-3"
+              src="https://emgotas.files.wordpress.com/2016/11/what-is-a-team.jpg"
+              alt="photo"
+            />
+          </div>
+          <div className="flex-grow-1 ms-3">
+            <h5>Blog post</h5>
+            <p>Lorem</p>
+          </div>
+        </div>
+
+        <div className="d-flex align-items-center me-5">
+          <div className="flex-shrink-0">
+            <img
+              width={150}
+              height={150}
+              className="mr-3"
+              src="https://emgotas.files.wordpress.com/2016/11/what-is-a-team.jpg"
+              alt="photo"
+            />
+          </div>
+          <div className="flex-grow-1 ms-3">
+            <h5>Blog post</h5>
+            <p>Lorem</p>
+          </div>
+        </div>
+      </Col>
+      <Col md="6">
+        <h5 className="text-center mt-5">Категорії</h5>
+
+        <Card>
+          <ListGroup variant="flush">
+            <ListGroup.Item>категорія 1</ListGroup.Item>
+            <ListGroup.Item>категорія 2</ListGroup.Item>
+            <ListGroup.Item>категорія 3</ListGroup.Item>
+            <ListGroup.Item>категорія 4</ListGroup.Item>
+            <ListGroup.Item>категорія 5</ListGroup.Item>
+          </ListGroup>
+        </Card>
+
+        <Card className="mt-3 bg-light">
+          <Card.Body>
+            <Card.Title>Slide widget</Card.Title>
+            <Card.Text>Lorem</Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  );
 };
